916b875150fe4c4b6b0f3f46fdfca63d78d408be
eslam985 <ee17172@gmail.com>
2026-09-25 13:07:17 +0300

    show all hover in lg screen only

--- src/app/[category]/[slug]/season/[season]/page.js
+++ src/app/[category]/[slug]/season/[season]/page.js
@@ -182,7 +182,7 @@ export default async function SeasonPage({ params }) {
             </div>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-6">
             {episodes?.map((ep, index) => (
               <Link
                 prefetch={false}
